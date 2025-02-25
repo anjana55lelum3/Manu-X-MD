2aa84584f25a545915babc1454a596c6ccdc3cf9
anjana55lelum3 <amarasekaralelumamarasekara@gmail.com>
2025-02-25 11:06:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&IIwCgL6B#Jj_lj65r3JxcThZmA8eOHMi1WqlHyGc29ZtL6I-Uv7Y' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
